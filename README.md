09dd6ea7e347b2661aca769d18fa2d2b89214356
Karl Hughes <khughes.me@gmail.com>
2017-11-09 12:22:53 -0600

    Adding node script

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# spc-site
-The static site for SideProjectChecklist.com
+# Side Project Checklist Static Site
+
+The static site generated for SideProjectChecklist.com
+
+## Local Development
+
+- Run the local environment (Docker): `npm run app:up`
+- Run the product list generator: `npm run generate:products`
+- Run the checklist copy script: `npm run generate:checklists`
---
+++ scripts/generate-products.js
@@ -0,0 +1,79 @@
+'use strict';
+const Airtable = require('airtable');
+const base = new Airtable({ apiKey: process.env.API_KEY }).base('appsWmNYoIAPDCz8V');
+const fs = require('fs');
+
+async function run() {
+  const categories = await (base('Categories').select().all());
+  const products = await (base('Products').select().all());
+
+  categories.forEach(category => {
+    let theseProducts = products.filter(product => {
+      if (product.fields.Category) {
+        return product.fields.Category.includes(category.id);
+      }
+    });
+
+    if (theseProducts && theseProducts.length) {
+      // Sort the products
+      theseProducts.sort((a, b) => {
+        if (a.fields['Vote Count'] === b.fields['Vote Count']) {
+          return (a.fields['Name'] < b.fields['Name']) ? -1 : (a.fields['Name'] > b.fields['Name']) ? 1 : 0;
+        } else {
+          return (a.fields['Vote Count'] > b.fields['Vote Count']) ? -1 : 1;
+        }
+      });
+      let markdownArray = generateMarkdown(theseProducts);
+      saveToFile(replaceAll(category.fields.Name, ' ', '-') + '.md', markdownArray);
+    }
+  });
+}
+
+function generateMarkdown(products) {
+  return products.map(product => {
+    return getLink(product) + '**[' + product.fields.Name + '](' + product.fields.URL + ')** ' + getPrice(product) + getDescription(product);
+  });
+}
+
+function getLink(product) {
+  return '- <a href="#vote-form" class="vote-link" rel="modal:open" id="' +
+    replaceAll(product.fields.Name, ' ', '_') +
+    '">&#x25B2; <span class="count">' +
+    product.fields['Vote Count'] +
+    '</span></a> &nbsp;';
+}
+
+function getDescription(product) {
+  return product.fields.Description ? ' - ' + product.fields.Description : '';
+}
+
+function getPrice(product) {
+  if (product.fields['Cheapest Plan'] || product.fields['Cheapest Plan'] === 0) {
+    return '<span style="color: grey;">($' + product.fields['Cheapest Plan'] + ')</span>'
+  }
+  return '';
+}
+
+function replaceAll(str, find, replace) {
+  return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
+}
+
+function escapeRegExp(str) {
+  return str.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
+}
+
+function saveToFile(filename, markdownArray) {
+  let path = "./results/" + filename;
+
+  // Truncate the file
+  if (fs.existsSync(path)) {
+    fs.truncateSync(path, 0);
+  }
+
+  // Add each line
+  markdownArray.forEach(markdownLine => {
+    fs.appendFileSync(path, markdownLine + '\r\n\r\n');
+  });
+}
+
+run();
