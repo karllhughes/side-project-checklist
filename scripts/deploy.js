dbe84f4336262fab3fc772cdc45dceef6d71e60d
Karl Hughes <khughes.me@gmail.com>
2018-04-10 18:05:08 -0500

    Automated commit

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,10 +2,11 @@ require('dotenv').config();
 const git = require('simple-git/promise');
 
 async function run() {
-  return await git().commit('Automated commit', '-a');
+  return await git()
+    .commit('Automated commit', '-a')
+    .push('origin', 'master');
 }
 
 run().then((err, result) => {
   console.log('Commit complete');
-  console.log(result);
 });
