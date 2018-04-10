00d2ba87000f8e5d9e530007dcb5430153acf2c1
Karl Hughes <khughes.me@gmail.com>
2018-04-10 16:47:55 -0500

    Testing

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const util = require('util');
 
-const stats = fs.statSync(path(__directory, '..', 'marketing-checklist.md'));
+const stats = fs.statSync(path.join(__dirname, '..', 'marketing-checklist.md'));
 const mtime = new Date(util.inspect(stats.mtime));
 console.log(mtime);
 
