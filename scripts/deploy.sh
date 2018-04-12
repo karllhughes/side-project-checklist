c2f9a3ea2d01076271d0c88e6056f78c84c11ec8
travis <travis>
2018-04-11 20:07:58 -0500

    init

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,7 @@ if [ -n "$GITHUB_API_KEY" ]; then
   git -c user.name='travis' -c user.email='travis' commit -m init
   # Make sure to make the output quiet, or else the API token will leak!
   # This works because the API key can replace your password.
-  git push -f -q https://karllhughes:$GITHUB_API_KEY@github.com/karllhughes/side-project-checklist-gh-pages gh-pages &2>/dev/null
+  git push -f -q https://karllhughes:$GITHUB_API_KEY@github.com/karllhughes/side-project-checklist gh-pages &2>/dev/null
   cd "$TRAVIS_BUILD_DIR"
   echo "Build and push complete."
 fi
