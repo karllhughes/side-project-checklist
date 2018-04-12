8b2f088e299e1c7e4be979d217851b96305cab52
Karl Hughes <khughes.me@gmail.com>
2018-04-11 20:15:12 -0500

    Updating deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,15 @@
 if [ -n "$GITHUB_API_KEY" ]; then
-  cd "$TRAVIS_BUILD_DIR"
-  git checkout -b gh-pages
-  git add .
+  # cd "$TRAVIS_BUILD_DIR"
+  cd web
+  git clone https://karllhughes:$GITHUB_API_KEY@github.com/karllhughes/side-project-checklist
+  git checkout -b deploy
+  npm install
+  npm run gen:products
+  npm run gen:checklists
+  git add -A
   git -c user.name='travis' -c user.email='travis' commit -m init
-  # Make sure to make the output quiet, or else the API token will leak!
-  # This works because the API key can replace your password.
-  git push -f -q https://karllhughes:$GITHUB_API_KEY@github.com/karllhughes/side-project-checklist gh-pages &2>/dev/null
-  cd "$TRAVIS_BUILD_DIR"
+  # git push -f -q https://karllhughes:$GITHUB_API_KEY@github.com/karllhughes/side-project-checklist gh-pages &2>/dev/null
+  # cd "$TRAVIS_BUILD_DIR"
   echo "Build and push complete."
 fi
 echo "Script complete."
