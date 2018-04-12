4051c1f31f6ee4f9224e7cc1b7b5718ebe638a5d
travis <travis>
2018-04-11 20:07:37 -0500

    init

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,12 @@
-npm install
-node scripts/deploy.js
+if [ -n "$GITHUB_API_KEY" ]; then
+  cd "$TRAVIS_BUILD_DIR"
+  git checkout -b gh-pages
+  git add .
+  git -c user.name='travis' -c user.email='travis' commit -m init
+  # Make sure to make the output quiet, or else the API token will leak!
+  # This works because the API key can replace your password.
+  git push -f -q https://karllhughes:$GITHUB_API_KEY@github.com/karllhughes/side-project-checklist-gh-pages gh-pages &2>/dev/null
+  cd "$TRAVIS_BUILD_DIR"
+  echo "Build and push complete."
+fi
+echo "No build"
