a8f47f5f351ada29f38d0210b2ba9b710f08b5d8
Karl Hughes <khughes.me@gmail.com>
2018-04-11 20:43:47 -0500

    Updating master deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,13 @@
 if [ -n "$GITHUB_API_KEY" ]; then
   cd "$TRAVIS_BUILD_DIR"
-  git clone -b deploy https://karllhughes:$GITHUB_API_KEY@github.com/karllhughes/side-project-checklist
+  git clone -b gh-pages https://karllhughes:$GITHUB_API_KEY@github.com/karllhughes/side-project-checklist
   cd side-project-checklist
-  git checkout -b deploy
   npm install
   AIRTABLE_API_KEY=$AIRTABLE_API_KEY AIRTABLE_BASE_ID=$AIRTABLE_BASE_ID node scripts/generate-products
   AIRTABLE_API_KEY=$AIRTABLE_API_KEY AIRTABLE_BASE_ID=$AIRTABLE_BASE_ID node scripts/generate-checklists
   git add -A
   git -c user.name='travis' -c user.email='travis' commit -m init
-  git push -f -q https://karllhughes:$GITHUB_API_KEY@github.com/karllhughes/side-project-checklist deploy &2>/dev/null
+  git push -f -q https://karllhughes:$GITHUB_API_KEY@github.com/karllhughes/side-project-checklist gh-pages &2>/dev/null
   cd "$TRAVIS_BUILD_DIR"
   echo "Build and push complete."
 fi
