be00007ed5a9c42246832cf4913b4e2c2e9aa6c8
Karl Hughes <khughes.me@gmail.com>
2018-09-01 17:04:02 -0500

    Updating target repo

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 if [ -n "$GITHUB_API_KEY" ]; then
   cd "$TRAVIS_BUILD_DIR"
-  git clone https://karllhughes:$GITHUB_API_KEY@github.com/karllhughes/side-project-checklist
+  git clone https://karllhughes:$GITHUB_API_KEY@github.com/portable-cto/side-project-checklist
   cd side-project-checklist
   git checkout gh-pages
   git merge master
@@ -9,7 +9,7 @@ if [ -n "$GITHUB_API_KEY" ]; then
   AIRTABLE_API_KEY=$AIRTABLE_API_KEY AIRTABLE_BASE_ID=$AIRTABLE_BASE_ID node scripts/generate-checklists
   git add -A
   git -c user.name='travis' -c user.email='travis' commit -m init
-  git push -f -q https://karllhughes:$GITHUB_API_KEY@github.com/karllhughes/side-project-checklist gh-pages &2>/dev/null
+  git push -f -q https://karllhughes:$GITHUB_API_KEY@github.com/portable-cto/side-project-checklist gh-pages &2>/dev/null
   cd "$TRAVIS_BUILD_DIR"
   echo "Build and push complete."
 fi
