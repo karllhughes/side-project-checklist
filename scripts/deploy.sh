4a36f1f44304758a1f19e640dcf953684fc5720a
Karl Hughes <khughes.me@gmail.com>
2020-12-19 13:53:00 -0600

    Removing buidls

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,8 +5,6 @@ if [ -n "$GITHUB_API_KEY" ]; then
   git checkout gh-pages
   git merge master
   npm install
-  AIRTABLE_API_KEY=$AIRTABLE_API_KEY AIRTABLE_BASE_ID=$AIRTABLE_BASE_ID node scripts/generate-products
-  AIRTABLE_API_KEY=$AIRTABLE_API_KEY AIRTABLE_BASE_ID=$AIRTABLE_BASE_ID node scripts/generate-checklists
   git add -A
   git -c user.name='travis' -c user.email='travis' commit -m init
   git push -f -q https://karllhughes:$GITHUB_API_KEY@github.com/portable-cto/side-project-checklist gh-pages &2>/dev/null
